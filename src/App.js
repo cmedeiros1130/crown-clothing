88e54c92b799bad0b9ee4087ffbf69fec63c5ec8
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-11-04 04:17:12 -0500

    organized components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./categories.styles.scss";
+import Directory from "./components/category-item/directory/directory.componet";
 
 const App = () => {
   const categories = [
@@ -29,23 +29,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-            }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
+
 export default App;
